5c3afd8b980364b091d534b139be12a13f1eaf4d
victor perrin <vperrin@Mac.yallo.box>
2025-06-01 14:30:52 +0200

    initial commit

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import requests
+from lxml import html
+from feedgen.feed import FeedGenerator
+from urllib.parse import urljoin
+
+URL = 'https://axelera.ai/careers'
+
+response = requests.get(URL)
+tree = html.fromstring(response.content)
+
+fg = FeedGenerator()
+fg.title('Axelera AI Careers')
+fg.link(href=URL)
+fg.description('Latest job offers at Axelera AI')
+
+# Get all h5 titles inside the "openings" section
+titles = tree.xpath('//div[contains(@id, "openings")]//h5')
+
+for h5 in titles:
+    title_text = h5.text_content().strip()
+
+    # Try to find the nearest <a> tag to get a link
+    link_el = h5.xpath('.//ancestor::a[1]/@href')
+    link = urljoin(URL, link_el[0]) if link_el else URL
+
+    # Optional: look for nearby <p> elements for description
+    description_parts = h5.xpath('./following-sibling::p[1]/text()')
+    description = description_parts[0].strip() if description_parts else ''
+
+    fe = fg.add_entry()
+    fe.title(title_text)
+    fe.link(href=link)
+    fe.description(description)
+
+# Output RSS
+rss_feed = fg.rss_str(pretty=True)
+print(rss_feed.decode())
